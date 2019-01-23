466d7618fe8d48c476f941056bc3b0fd49c4801a
ShraddhaAg <shraddha.agrawal000@gmail.com>
2019-01-23 20:58:55 +0530

    Add Task List

--- ToDo/tasks/views.py
+++ ToDo/tasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from tasks.models import task, hashtag
+from django.views import generic
 
-# Create your views here.
+class Task(generic.ListView):
+    model = task
