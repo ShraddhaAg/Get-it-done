bcb022a22aca13ea495a9667711decd888724759
ShraddhaAg <shraddha.agrawal000@gmail.com>
2019-01-27 21:40:22 +0530

    Modify add-category UI

--- ToDo/tasks/forms.py
+++ ToDo/tasks/forms.py
@@ -1,10 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import hashtag, task
 
 class AddCategory(ModelForm):
     class Meta:
         model = hashtag
         fields = ['category']
+        # widgets = {
+        #     'category': TextInput(attrs={'class': 'input', 'placeholder': 'Enter the category name'}),
+        # }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs):
+        self.fields['category'].widget.attrs['class'] = 'input'
 
 class AddTask(ModelForm):
     class Meta:
